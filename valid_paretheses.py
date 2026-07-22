47fe9859aa7dc45d50024c65aee250148e092264
Virendra <virendraagale2803@gmail.com>
2026-07-22 18:20:33 +0530

    new question

---
+++ valid_paretheses.py
@@ -0,0 +1,18 @@
+s = "()[(]{}"
+
+def vaild_para(s):
+    map = { "}":"{","]":"[",")":"("}
+
+    stack = []
+    for i in s:
+        if i in map.values():
+            stack.append(i)
+        elif i in map:
+            if not stack or stack[-1] != map[i]:
+                return False
+            stack.pop()
+        else:
+            return False
+    return not stack
+
+print(vaild_para(s))
